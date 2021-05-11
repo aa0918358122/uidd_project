0cf1bed8027fb12a685baae137cbb5a5454b6baa
aa0918358122 <aa0918358122@gmail.com>
2021-05-12 01:26:01 +0800

    v5

--- profile.js
+++ profile.js
@@ -19,73 +19,126 @@ $(document).ready(function() {
     $('#save').css('opacity', 1);
     $('#down').css('opacity', 1);
   })
-  var height = $("#more_question").offset().top;
-  $('#more_answer').css('top', height);
-  $('#save').css('top', height);
   $('#a').click(function() {
-    $('#more_answer').css('position', 'absolute');
-    $('#more_question').css('position', 'fixed');
-    $('#save').css('position', 'fixed');
-    if($('#q').css('background-color', '#ddd')) {
-      $('#more_question').animate({left: "-100vw"}, 200);
-      $('#more_answer').animate({left: 0}, 200);
-      $('#save').animate({left: "100vw"}, 200);
+    if($('#q').css('background-color', '#f5f5f5')) {
+      $('.more').css('transform', 'translate(-100vw, 0)');
       $('#q').css('background-color', 'white');
       $('#s').css('background-color', 'white');
-      $('#a').css('background-color', '#ddd');
+      $('#a').css('background-color', '#f5f5f5');
+      $('#q').css('border', 'none');
+      $('#q').css('border-bottom', '0.5vh dashed #7874CD');
+      $('#s').css('border', 'none');
+      $('#s').css('border-bottom', '0.5vh dashed #7874CD');
+      $('#a').css('border', '0.5vh dashed #F2692C');
     }
-    else if($('#s').css('background-color', '#ddd')) {
-      $('#save').animate({left: "100vw"}, 200);
-      $('#more_answer').animate({left: 0}, 200);
-      $('#more_question').animate({left: "-100vw"}, 200);
+    else if($('#s').css('background-color', '#f5f5f5')) {
+      $('.more').css('transform', 'translate(-100vw, 0)');
       $('#q').css('background-color', 'white');
       $('#s').css('background-color', 'white');
-      $('#a').css('background-color', '#ddd');
+      $('#a').css('background-color', '#f5f5f5');
+      $('#q').css('border', 'none');
+      $('#q').css('border-bottom', '0.5vh dashed #7874CD');
+      $('#s').css('border', 'none');
+      $('#s').css('border-bottom', '0.5vh dashed #7874CD');
+      $('#a').css('border', '0.5vh dashed #F2692C');
     }
   })
   $('#q').click(function() {
-    $('#more_question').css('position', 'absolute');
-    $('#more_answer').css('position', 'fixed');
-    $('#save').css('position', 'fixed');
-    if($('#a').css('background-color', '#ddd')) {
-      $('#save').animate({left: "100vw"}, 200);
-      $('#more_answer').animate({left: "100vw"}, 200);
-      $('#more_question').animate({left: 0}, 200);
+    if($('#a').css('background-color', '#f5f5f5')) {
+      $('.more').css('transform', 'translate(0, 0)');
       $('#a').css('background-color', 'white');
       $('#s').css('background-color', 'white');
-      $('#q').css('background-color', '#ddd');
+      $('#q').css('background-color', '#f5f5f5');
+      $('#a').css('border', 'none');
+      $('#a').css('border-bottom', '0.5vh dashed #7874CD');
+      $('#s').css('border', 'none');
+      $('#s').css('border-bottom', '0.5vh dashed #7874CD');
+      $('#q').css('border', '0.5vh dashed #F2692C');
     }
-    else if($('#s').css('background-color', '#ddd')) {
-      $('#save').animate({left: "100vw"}, 200);
-      $('#more_answer').animate({left: "100vw"}, 200);
-      $('#more_question').animate({left: 0}, 200);
+    else if($('#s').css('background-color', '#f5f5f5')) {
+      $('.more').css('transform', 'translate(0, 0)');
       $('#s').css('background-color', 'white');
       $('#a').css('background-color', 'white');
-      $('#q').css('background-color', '#ddd');
+      $('#q').css('background-color', '#f5f5f5');
+      $('#a').css('border', 'none');
+      $('#a').css('border-bottom', '0.5vh dashed #7874CD');
+      $('#s').css('border', 'none');
+      $('#s').css('border-bottom', '0.5vh dashed #7874CD');
+      $('#q').css('border', '0.5vh dashed #F2692C');
     }
   })
   $('#s').click(function() {
-    $('#save').css('position', 'absolute');
-    $('#more_answer').css('position', 'fixed');
-    $('#more_question').css('position', 'fixed');
-    if($('#q').css('background-color', '#ddd')) {
-      $('#more_question').animate({left: "-100vw"}, 200);
-      $('#more_answer').animate({left: "-100vw"}, 200);
-      $('#save').animate({left: 0}, 200);
+    if($('#q').css('background-color', '#f5f5f5')) {
+      $('.more').css('transform', 'translate(-200vw, 0)');
       $('#q').css('background-color', 'white');
       $('#a').css('background-color', 'white');
-      $('#s').css('background-color', '#ddd');
+      $('#s').css('background-color', '#f5f5f5');
+      $('#a').css('border', 'none');
+      $('#a').css('border-bottom', '0.5vh dashed #7874CD');
+      $('#q').css('border', 'none');
+      $('#q').css('border-bottom', '0.5vh dashed #7874CD');
+      $('#s').css('border', '0.5vh dashed #F2692C');
     }
     else if($('#a').css('background-color', '#ddd')) {
-      $('#more_answer').animate({left: "-100vw"}, 200);
-      $('#more_question').animate({left: "-100vw"}, 200);
-      $('#save').animate({left: 0}, 200);
+      $('.more').css('transform', 'translate(-200vw, 0)');
       $('#a').css('background-color', 'white');
       $('#q').css('background-color', 'white');
-      $('#s').css('background-color', '#ddd');
+      $('#s').css('background-color', '#f5f5f5');
+      $('#a').css('border', 'none');
+      $('#a').css('border-bottom', '0.5vh dashed #7874CD');
+      $('#q').css('border', 'none');
+      $('#q').css('border-bottom', '0.5vh dashed #7874CD');
+      $('#s').css('border', '0.5vh dashed #F2692C');
     }
   })
+
+  $('#more_question').on('swipeleft', function(){
+    $('.more').css('transform', 'translate(-100vw, 0)');
+    $('#q').css('background-color', 'white');
+    $('#s').css('background-color', 'white');
+    $('#a').css('background-color', '#f5f5f5');
+    $('#q').css('border', 'none');
+    $('#q').css('border-bottom', '0.5vh dashed #7874CD');
+    $('#s').css('border', 'none');
+    $('#s').css('border-bottom', '0.5vh dashed #7874CD');
+    $('#a').css('border', '0.5vh dashed #F2692C');
+  })
+  $('#more_answer').on('swipeleft', function(){
+    $('.more').css('transform', 'translate(-200vw, 0)');
+    $('#q').css('background-color', 'white');
+    $('#a').css('background-color', 'white');
+    $('#s').css('background-color', '#f5f5f5');
+    $('#q').css('border', 'none');
+    $('#q').css('border-bottom', '0.5vh dashed #7874CD');
+    $('#a').css('border', 'none');
+    $('#a').css('border-bottom', '0.5vh dashed #7874CD');
+    $('#s').css('border', '0.5vh dashed #F2692C');
+  })
+  $('#more_answer').on('swiperight', function(){
+    $('.more').css('transform', 'translate(0, 0)');
+    $('#a').css('background-color', 'white');
+    $('#s').css('background-color', 'white');
+    $('#q').css('background-color', '#f5f5f5');
+    $('#a').css('border', 'none');
+    $('#a').css('border-bottom', '0.5vh dashed #7874CD');
+    $('#s').css('border', 'none');
+    $('#s').css('border-bottom', '0.5vh dashed #7874CD');
+    $('#q').css('border', '0.5vh dashed #F2692C');
+  })
+  $('#save').on('swiperight', function(){
+    $('.more').css('transform', 'translate(-100vw, 0)');
+    $('#q').css('background-color', 'white');
+    $('#s').css('background-color', 'white');
+    $('#a').css('background-color', '#f5f5f5');
+    $('#q').css('border', 'none');
+    $('#q').css('border-bottom', '0.5vh dashed #7874CD');
+    $('#s').css('border', 'none');
+    $('#s').css('border-bottom', '0.5vh dashed #7874CD');
+    $('#a').css('border', '0.5vh dashed #F2692C');
+  })
+
 });
+
 
 var tiger = document.getElementById('tiger');
 var animItem = bodymovin.loadAnimation({
@@ -151,50 +204,3 @@ var animItem = bodymovin.loadAnimation({
   path: 'https://assets7.lottiefiles.com/packages/lf20_z9ed2jna.json'
 });
 
-$("#Tiger").hover(function() {
-  $("#tiger").css("opacity", "0.5");
-  $("#Tiger span").show();
-}, function() {
-  $("#tiger").css("opacity", "1.0");
-  $("#Tiger span").hide();
-});
-
-$("#Panda").hover(function() {
-  $("#panda").css("opacity", "0.5");
-  $("#Panda span").show();
-}, function() {
-  $("#panda").css("opacity", "1.0");
-  $("#Panda span").hide();
-});
-
-$("#Monkey").hover(function() {
-  $("#monkey").css("opacity", "0.5");
-  $("#Monkey span").show();
-}, function() {
-  $("#monkey").css("opacity", "1.0");
-  $("#Monkey span").hide();
-});
-
-$("#Dog").hover(function() {
-  $("#dog").css("opacity", "0.5");
-  $("#Dog span").show();
-}, function() {
-  $("#dog").css("opacity", "1.0");
-  $("#Dog span").hide();
-});
-
-$("#Platypus").hover(function() {
-  $("#platypus").css("opacity", "0.5");
-  $("#Platypus span").show();
-}, function() {
-  $("#platypus").css("opacity", "1.0");
-  $("#Platypus span").hide();
-});
-
-$("#Otter").hover(function() {
-  $("#otter").css("opacity", "0.5");
-  $("#Otter span").show();
-}, function() {
-  $("#otter").css("opacity", "1.0");
-  $("#Otter span").hide();
-});
